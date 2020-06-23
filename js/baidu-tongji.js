72fdfce455a3f90f11cc270ee594c7e1770a2953
zhangyanjiong <zhangyj@mjoygames.com>
2020-06-23 14:34:04 +0800

    修改统计代码，根据不同的域名加载。

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -1,7 +1,14 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5f129d8c80eb327456a12090dac450f4";
+  
+  var domain = window.location.host;
+  if (domain == 'zhangyanjiong.com' || domain == 'www.zhangyanjiong.com') {
+      hm.src = "https://hm.baidu.com/hm.js?5f129d8c80eb327456a12090dac450f4";
+  } else if (domain == 'devzyj.com' || domain == 'www.devzyj.com') {
+      hm.src = "https://hm.baidu.com/hm.js?9ae93ab7763cccf298bdb0719b50f053";
+  }
+  
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
