009f1551134bd008413c6b27276880aa11d151cb
zhangyanjiong <zhangyj@mjoygames.com>
2020-06-23 11:29:32 +0800

    初始化 个人导航站

---
+++ js/baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5f129d8c80eb327456a12090dac450f4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
